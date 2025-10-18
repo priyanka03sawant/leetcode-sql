e28d5eef91b043a671abfd3eb22b436ffb6c6cdf
priyanka03sawant <priyanka03sawant@gmail.com>
2025-10-18 13:23:24 +0530

    Create 1729 — Find Followers Count.sql

---
+++ Easy/1729 — Find Followers Count.sql
@@ -0,0 +1,9 @@
+-- Problem: 1729. Find Followers Count
+-- https://leetcode.com/problems/find-followers-count/
+
+# Write your MySQL query statement below
+SELECT user_id,
+       COUNT(follower_id) AS followers_count
+FROM Followers
+GROUP BY user_id
+ORDER BY user_id ASC;
